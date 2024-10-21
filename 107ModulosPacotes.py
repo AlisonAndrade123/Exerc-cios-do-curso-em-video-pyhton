f1ca42d53e075896aaf4c11bed50afb41654561f
AlisonAndrade123 <andradealison291@gmail.com>
2024-10-21 09:04:05 -0300

    mandando atividades 23

---
+++ 107ModulosPacotes.py
@@ -0,0 +1,7 @@
+#import Uteis
+from Uteis import fatorial, dobro
+
+num = int(input('Digite um valor: '))
+fat = fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {dobro(num)}')
